91df25c2f698a2c47109105f4686db99ba7294fd
bialamalpa95 <biala.malpa95@gmail.com>
2026-05-27 12:39:40 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'lubicz-v2';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'Logo XXI LDH.png'
+];
+
+// Instalacja i zapisywanie plików w pamięci podręcznej
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Serwowanie plików z pamięci podręcznej, gdy brak sieci
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
